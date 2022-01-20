76f73833ca094ea8d9a55e4be9c3770a767db601
Julia <julia.gray30@gmail.com>
2022-01-20 08:43:59 +0000

    eight am local storage adding

--- Develop/style.js
+++ Develop/style.js
@@ -1,5 +1,5 @@
 const eightBttn = document.getElementById("eightBttn")
-const eightInput = document.getElementById("#eightInput");
+// const eightInput = document.getElementById("#eightInput");
 const eightRow = document.getElementById("eightRow")
 const nineRow = document.getElementById("nineRow");
 const tenRow = document.getElementById("tenRow");
@@ -11,26 +11,31 @@ const threeRow = document.getElementById("threeRow");
 const fourRow = document.getElementById("fourRow");
 const fiveRow = document.getElementById("fiveRow");
 let timeNow = moment().format("H")
+console.log(timeNow)
 
 
-saveUserInput = (e) => {
-    e.preventDefault();
-    console.log("clicked")
-    userEightInput = eightInput.value;
-    localStorage.setItem("userInput", userEightInput)
-    console.log(userEightInput)
-}
+checkTime();
+// eightTextLoad();
 
-// eightBttn.click(saveUserInput)
 
-// function saveUserInput () {
-//     let userInput = eightInput.value
-//     localStorage.setItem(userInput, userInput)
+// eightBttn.addEventListener("click", saveInput())
+
+// function saveInput(){
+//     preventDefault()
+//     let userInput = eightInput.value;
 //     console.log(userInput)
 // }
 
-// // let userInput = $("#9am").value
-// // localStorage.setItem("userInput", userInput)
+function store() {
+    let input = document.getElementById("eightInput")
+    localStorage.setItem("eightAmInput", eightInput.value)
+}
+
+// function eightTextLoad(){
+//     let eightload = localStorage.getItem("eightAmInput")
+//     document.getElementById("eightInput value").textContent = eightload
+// }
+
 
 
 // The time -------------------------------------------------
@@ -40,11 +45,10 @@ console.log(moment());
 $("#currentDay").text(reformatDate);
 
 
-
 // Row colours change depending on time ---------------------
 // 8am
 function checkTime() {
-  if (timeNow === 8) {
+  if (timeNow == 8) {
     eightRow.classList.add("present");
   } else if (timeNow > 8) {
     eightRow.classList.add("past");
@@ -52,7 +56,7 @@ function checkTime() {
     eightRow.classList.add("future");
   }
 // 9am
-  if (timeNow === 9) {
+  if (timeNow == 9) {
       nineRow.classList.add("present");
   } else if (timeNow > 9) {
       nineRow.classList.add("past")
@@ -60,7 +64,7 @@ function checkTime() {
       nineRow.classList.add("future")
   }
 // 10am
-  if (timeNow === 10) {
+  if (timeNow == 10) {
     tenRow.classList.add("present");
 } else if (timeNow > 10) {
     tenRow.classList.add("past")
@@ -68,7 +72,7 @@ function checkTime() {
     tenRow.classList.add("future")
 }
 //11am 
-if (timeNow === 11) {
+if (timeNow == 11) {
     elevenRow.classList.add("present");
 } else if (timeNow > 11) {
     elevenRow.classList.add("past")
@@ -76,7 +80,7 @@ if (timeNow === 11) {
     elevenRow.classList.add("future")
 }
 // 12pm
-if (timeNow === 12) {
+if (timeNow == 12) {
     twelveRow.classList.add("present");
 } else if (timeNow > 12) {
     twelveRow.classList.add("past")
@@ -84,7 +88,7 @@ if (timeNow === 12) {
     twelveRow.classList.add("future")
 }
 // 1pm
-if (timeNow === 13) {
+if (timeNow == 13) {
     oneRow.classList.add("present");
 } else if (timeNow > 13) {
     oneRow.classList.add("past")
@@ -92,7 +96,7 @@ if (timeNow === 13) {
     oneRow.classList.add("future")
 }
 // 2pm
-if (timeNow === 14) {
+if (timeNow == 14) {
     twoRow.classList.add("present");
 } else if (timeNow > 14) {
     twoRow.classList.add("past")
@@ -100,7 +104,7 @@ if (timeNow === 14) {
     twoRow.classList.add("future")
 }
 // 3pm
-if (timeNow === 15) {
+if (timeNow == 15) {
     threeRow.classList.add("present");
 } else if (timeNow > 15) {
     threeRow.classList.add("past")
@@ -108,7 +112,7 @@ if (timeNow === 15) {
     threeRow.classList.add("future")
 }
 // 4pm
-if (timeNow === 16) {
+if (timeNow == 16) {
     fourRow.classList.add("present");
 } else if (timeNow > 16) {
     fourRow.classList.add("past")
@@ -116,17 +120,13 @@ if (timeNow === 16) {
     fourRow.classList.add("future")
 }
 // 5pm
-if (timeNow === 17) {
+if (timeNow == 17) {
     fiveRow.classList.add("present");
 } else if (timeNow > 17) {
     fiveRow.classList.add("past")
 } else if (timeNow < 17) {
     fiveRow.classList.add("future")
 } }
-
-
-
-checkTime();
 
 
 
